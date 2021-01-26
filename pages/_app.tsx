359c739309beb20512486e270c528f967e5018d6
Nadno <eunadno@gmail.com>
2021-01-25 23:03:05 -0300

    ThemeProvider and GlobalStyle created

---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import { createGlobalStyle, StyledProps, ThemeProvider } from "styled-components";
+
+import db from '../db.json';
+import { QuizTheme } from "../interfaces/theme";
+
+const GlobalStyle = createGlobalStyle<{ theme: QuizTheme }>`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const theme: QuizTheme = db.theme;
+
+
+export default function App({ Component, pageProps }: StyledProps<any>) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component { ...pageProps } />
+      </ThemeProvider>
+    </>
+  );
+};
