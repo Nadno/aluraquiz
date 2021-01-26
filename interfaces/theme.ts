43d0a8c1b6eaacfba9b7a425403b9f8e291f3154
Nadno <eunadno@gmail.com>
2021-01-25 23:04:40 -0300

    theme interface

---
+++ interfaces/theme.ts
@@ -0,0 +1,11 @@
+export interface QuizTheme {
+  colors: {
+    primary: string;
+    secondary: string;
+    mainBg: string;
+    contrastText: string;
+    wrong: string;
+    success: string;
+  },
+  borderRadius: string;
+}
