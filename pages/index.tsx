aefea967b6d07ff7bb101205f00b4f8ff26b472f
Nadno <eunadno@gmail.com>
2021-01-26 20:07:36 -0300

    add meta tag og:image

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const IndexPage = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
 
-      <meta property="og:image" content={db.bg} key="image" />
+      <meta property="og:image" content={db.bg} key="ogimage" />
 
       <title>LoremQuiz</title>
     </Head>
