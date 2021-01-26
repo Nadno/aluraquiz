ccef97efefcf5667bbe58b610befbe8cd1680d61
Nadno <eunadno@gmail.com>
2021-01-26 18:01:33 -0300

    theme interface changed for declared module DefaultTheme

--- components/BackGround.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import db from "../db.json";
-
-const BackgroundImage = styled.div`
-  background-image: url('${db.bg}');
-  height: 100vh;
-`;
-
-export default BackgroundImage;
