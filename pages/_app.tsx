1a3fd698c04a73de00836316b8420eb1bbdd28d0
Nadno <eunadno@gmail.com>
2021-01-27 05:48:40 -0300

    added theme christmas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,10 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    font-size: 62.5%;
+  }
+
   body {
     margin: 0;
     padding: 0;
@@ -31,7 +35,7 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const theme = db.theme;
+const theme = db.theme.default;
 
 
 export default function App({ Component, pageProps }: StyledProps<any>) {
