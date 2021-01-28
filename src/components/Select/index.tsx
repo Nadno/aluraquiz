88f55a2076d70fc347bdcb982398edd45f7c8fd3
Nadno <eunadno@gmail.com>
2021-01-28 06:18:35 -0300

    created Select component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,84 @@
+import { ChangeEvent, useState } from 'react';
+import styled from 'styled-components';
+
+const SelectStyle = styled.div`
+  input {
+    position: absolute;
+    display: none;
+  }
+
+  label {
+    position: relative;
+    display: block;
+
+    width: 100%;
+    padding: 1rem 1.5rem;
+    border: 1px solid ${({ theme }) => theme.colors.mainBg};
+    border-radius: ${({ theme }) => theme.borderRadius};
+    color: ${({ theme }) => theme.colors.contrastText};
+    background-color: ${({ theme }) => theme.colors.primary};
+
+    margin-bottom: 1rem;
+
+    font-size: 1.5rem;
+    font-weight: 600;
+
+    &.success {
+      background-color: ${({ theme }) => theme.colors.success};
+    }
+
+    &.wrong {
+      background-color: ${({ theme }) => theme.colors.wrong};
+    }
+
+    &:hover {
+      opacity: 0.6;
+      cursor: pointer;
+    }
+  }
+`;
+
+interface Props {
+  alternatives: string[];
+  questionId: number;
+  answer: number;
+  handleAnswer(name: string, value: boolean): void;
+}
+
+const Select = ({ alternatives, answer, questionId, handleAnswer }: Props) => {
+  const [selectAlt, setSelectAlt] = useState(false);
+
+  const handleCheck = (e: ChangeEvent) => {
+    if (selectAlt) return;
+    const target = e.target as HTMLInputElement;
+    const { name, value, nextElementSibling } = target;
+    const result = Number(value) === answer;
+
+    (nextElementSibling as HTMLInputElement).classList.add(
+      result ? 'success' : 'wrong'
+    );
+
+    handleAnswer(name, result);
+    setSelectAlt(true);
+  };
+
+  const getSelectOptions = (alt: string, key: number) => {
+    const altKey = `option-${key}`;
+    return (
+      <SelectStyle key={altKey}>
+        <input
+          id={altKey}
+          value={key}
+          type="radio"
+          name={`question__${questionId}`}
+          onChange={handleCheck}
+        />
+        <label htmlFor={altKey}>{alt}</label>
+      </SelectStyle>
+    );
+  };
+
+  return <div>{alternatives.map(getSelectOptions)}</div>;
+};
+
+export default Select;
