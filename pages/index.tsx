596cc5696edb50af3d52b797d4b7a017463d908c
Nadno <eunadno@gmail.com>
2021-01-28 06:17:58 -0300

    added user state, name input and form submit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,90 @@
-import React from "react";
-import Head from "next/head";
-import db from "../db.json";
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import db from '../db.json';
 
-import QuizBackground from "../src/components/QuizBackground";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
-import QuizLogo from "../src/components/QuizLogo";
-import Widget from "../src/components/Widget";
-import InputText from "../src/components/InputText";
-import Button from "../src/components/Button";
-import QuizContainer from "../src/components/QuizContainer";
+import QuizBackground from '../src/layouts/QuizBackground';
+import QuizContainer from '../src/layouts/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Footer from '../src/components/Footer';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
-const IndexPage = () => (
-  <QuizBackground backgroundImage={db.bg}>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
+const IndexPage = () => {
+  const [user, setUser] = useState('');
+  const router = useRouter();
 
-      <meta property="og:image" content={db.bg} key="ogimage" />
-      <meta
-        property="og:description"
-        content="Descubra quem você seria em The Last of Us"
-        key="ogdescription"
-      />
+  const handleChange = (e: ChangeEvent) => {
+    const { value } = e.target as HTMLInputElement;
+    setUser(value);
+  };
 
-      <title>The Last of Us - Quiz</title>
-    </Head>
-    <QuizContainer>
-      <QuizLogo className="logo" />
-      <Widget>
-        <Widget.Header>
-          <h1>The Last of Us</h1>
-          {/* <Widget.Christmas src="https://cdn.pixabay.com/photo/2021/01/03/23/40/christmas-5885920_1280.png" /> */}
-        </Widget.Header>
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
 
-        <Widget.Content>
-          <p>Descubra quem você seria em The Last of Us.</p>
+    router.push(`/quiz?name=${user}`);
+  };
 
-          <InputText
-            id="name"
-            name="name"
-            placeholder="Digite um nome para jogar :)"
-          />
+  const isDisabled = !user.trim();
+  
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <meta property="og:image" content={db.bg} key="ogimage" />
+        <meta
+          property="og:description"
+          content="Descubra o quanto você conhece javascript"
+          key="ogdescription"
+        />
 
-          <Button />
-        </Widget.Content>
-      </Widget>
+        <title>Javascript - Quiz</title>
+      </Head>
 
-      <Widget>
-        <Widget.Content>
-          <h1>Quizes da Galera</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, quam.
-          </p>
-        </Widget.Content>
-      </Widget>
+      <QuizContainer>
+        <QuizLogo className="logo" />
+        <Widget>
+          <Widget.Header>
+            <h1>Javascript</h1>
+            {/* <Widget.Christmas src="https://cdn.pixabay.com/photo/2021/01/03/23/40/christmas-5885920_1280.png" /> */}
+          </Widget.Header>
 
-      <Footer />
-    </QuizContainer>
+          <Widget.Content>
+            <p>Descubra o quanto você conhece javascript.</p>
 
-    <GitHubCorner projectUrl="https://github.com/Nadno/aluraquiz" />
-  </QuizBackground>
-);
+            <form onSubmit={handleSubmit}>
+              <Input.Text
+                id="name"
+                name="name"
+                value={user}
+                onChange={handleChange}
+                placeholder="Digite um nome para jogar :)"
+              />
+
+              <Button type="submit" disabled={isDisabled}>
+                JOGAR
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel,
+              quam.
+            </p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+      </QuizContainer>
+
+      <GitHubCorner projectUrl="https://github.com/Nadno/aluraquiz" />
+    </QuizBackground>
+  );
+};
 
 export default IndexPage;
