48621cdf08415eac4f871b6f45dd1b1c67573bac
Nadno <eunadno@gmail.com>
2021-01-29 07:28:45 -0300

    added getServerSideProps

--- pages/quiz/[id].tsx
+++ pages/quiz/[id].tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import Quiz from '../../src/screens/Quiz';
 
-const GuysQUiz = () => {
+import { ExternalDB } from '../../src/interfaces/db';
+import { NextPageContext } from 'next';
+import { ThemeProvider } from 'styled-components';
+
+export default function GuysQuiz({ externalDB }: { externalDB: ExternalDB }) {
   return (
-    <div>
-      Teste
-    </div>
+    <ThemeProvider theme={externalDB.theme}>
+      <Quiz quiz={externalDB} />
+    </ThemeProvider>
   );
-};
+}
 
-export default GuysQUiz;
+export async function getServerSideProps(context: NextPageContext) {
+
+  try {
+    const [repoName, name] = ((context.query.id as unknown) as string).split(
+      '___'
+    );
+    const externalDB = await fetch(
+      `https://${repoName}.${name}.vercel.app/api/db`
+    )
+      .then((res) => (res.ok ? res.json() : null))
+      .then((res) => res);
+      
+    return {
+      props: {
+        externalDB,
+      },
+    };
+  } catch (err) {
+    useRouter().push('/');
+  }
+}
